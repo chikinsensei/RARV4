3a30e46b394ef4c5863e98bba2c683f2533580a3
Thomas <73823655+chikinsensei@users.noreply.github.com>
2024-01-26 12:26:08 +0100

    1st part done

---
+++ Assets/WS-AR-tour/Scripts/ItemLabelBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemLabelBehaviour : MonoBehaviour {
+
+    public Text Label;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //TODO always turn the label to the Camera
+        
+	}
+
+    public void UpdatePositionAndLabel(Transform labelPositionTransform, string label = "")
+    {
+        //TODO update position of the label and label value
+        
+    }
+}
